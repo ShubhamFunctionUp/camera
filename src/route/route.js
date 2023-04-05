56016b42144fc36e372a025eeef2a53ddbebe6d1
ShubhamFunctionUp <shubham9638464483@gmail.com>
2023-04-06 00:38:39 +0530

    Route is defined

--- src/route/route.js
+++ src/route/route.js
@@ -4,10 +4,16 @@ const cameraController = require('../controller/camera');
 const cameraNetworkController = require('../controller/cameraNetworks');
 
 // Cameras
-router.get('/', cameraController.getAllCamera)
-router.get('/:id',cameraController.getSpecificCamera)
-router.post('/',cameraController.addCamera)
-router.put('/:id',cameraController.updateCamera)
-router.delete('/:id',cameraController.deleteCamera)
+router.get('/getAllCamera', cameraController.getAllCamera)
+router.get('/getCamera/:id',cameraController.getSpecificCamera)
+router.post('/addCamera',cameraController.addCamera)
+router.put('/camera/:id',cameraController.updateCamera)
+router.delete('/camera/:id',cameraController.deleteCamera)
+
+router.get('/getAllCameraNetwork', cameraNetworkController.getAllCameraNetworks)
+router.get('/getCameraNetwork/:id',cameraNetworkController.getCameraNetworkById)
+router.post('/addCameraNetwork',cameraNetworkController.addCameraNetwork)
+router.put('/cameraNetwork/:id',cameraNetworkController.updateCameraNetwork)
+router.delete('/cameraNetwork/:id',cameraNetworkController.deleteCameraNetwork)
 
 module.exports = router;
